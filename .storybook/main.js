a42aeee451e080f25575f0f4ad961e21d97444d8
dannyhw <dannyhyunsoowilliams@gmail.com>
2021-12-08 13:30:30 +0000

    repro: reanimated and gesture handler setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,20 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-react-native-web"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-react-native-web',
+      options: {
+        modulesToTranspile: ['react-native-reanimated'],
+        babelPlugins: ['react-native-reanimated/plugin'],
+      },
+    },
   ],
-  "framework": "@storybook/react"
-}
+  framework: '@storybook/react',
+};
